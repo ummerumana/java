5cfa0bd40642b2e7316a37668aee01c4ed92db61
ummerumana <umme.rumana@razorthink.com>
2020-02-12 18:19:10 +0530

    added programs from assignment 1

--- src/main/java/com/razorthink/java/program2/Program2.java
+++ src/main/java/com/razorthink/java/program2/Program2.java
@@ -6,23 +6,19 @@ public class Program2 {
     public Program2() {
     }
 
-    public boolean main() {
-
-        Rectangle[] r= new Rectangle[3];
-       r[0] = new Rectangle(10,12);
-        r[1] = new Rectangle(20,10);
-        r[2] = new Rectangle(30,11);
+    public double main(int x,int y) {
 
 
+       Rectangle r = new Rectangle(x,y);
 
 
         System.out.println("Rectangle\tlength\t\tbreadth\t\tarea\t\tperemeter\n-----------------------------------------------------------");
 
-        for(int i = 0; i < r.length; ++i) {
-            System.out.println((i+1)+"\t\t"+r[i].getLength() + "\t\t" + r[i].getBreadth() + "\t\t" + r[i].areaOfRectangle() + "\t\t" + r[i].peremeterOfRectangle());
-        }
 
-        return true;
+            System.out.println((1)+"\t\t"+r.getLength() + "\t\t" + r.getBreadth() + "\t\t" + r.areaOfRectangle() + "\t\t" + r.peremeterOfRectangle());
+
+
+        return r.areaOfRectangle();
     }
 
 
--- src/test/java/com/razorthink/java/test/Program2Test.java
+++ src/test/java/com/razorthink/java/test/Program2Test.java
@@ -3,13 +3,14 @@ package com.razorthink.java.test;
 import com.razorthink.java.program2.Program2;
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 public class Program2Test {
         @Test
         public void test() {
             Program2 p = new Program2();
-           assertTrue(p.main());
+           int res =(int) p.main(10,2);
+           assertEquals(20,res);
         }
 
 }
